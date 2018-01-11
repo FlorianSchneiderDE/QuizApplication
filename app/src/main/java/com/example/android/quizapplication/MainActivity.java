889a4336e5c2abe91c7819b21a69561535d24c6b
Florian Schneider <schneider@mathematik.uni-kl.de>
2018-01-11 13:29:35 +0100

    Multiple choice support, mehrere Fragen mit Bildern möglich

--- app/src/main/java/com/example/android/quizapplication/MainActivity.java
+++ app/src/main/java/com/example/android/quizapplication/MainActivity.java
@@ -1,19 +1,28 @@
 package com.example.android.quizapplication;
 
 import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     String[] buttonStatus = new String[4];
     int[] idList = new int[4];
     int currentQuestion = 0;
+    int numberOfQuestions;
     Resources res;
     String[] questionType;
     String[] correctAnswers;
+    String[] questions;
+    String[] answersA,answersB,answersC,answersD;
+    String solutionState;
+    TypedArray images;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +39,14 @@ public class MainActivity extends AppCompatActivity {
         res = getResources();
         questionType = res.getStringArray(R.array.questionType);
         correctAnswers = res.getStringArray(R.array.correctAnswers);
+        questions = res.getStringArray(R.array.questions);
+        answersA = res.getStringArray(R.array.answersA);
+        answersB = res.getStringArray(R.array.answersB);
+        answersC = res.getStringArray(R.array.answersC);
+        answersD = res.getStringArray(R.array.answersD);
+        solutionState = "solution";
+        numberOfQuestions = correctAnswers.length;
+        images = res.obtainTypedArray(R.array.pictures);
     }
 
     public void changeStatusOfButton(View v) {
@@ -50,40 +67,90 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void updateUI(View v) {
-        for(int i=0;i<4;i++){
-            switch (buttonStatus[i]) {
-                case "none":
-                    (findViewById(idList[i])).setBackgroundResource(R.drawable.buttonshape);
-                    break;
-                case "active":
-                    (findViewById(idList[i])).setBackgroundResource(R.drawable.buttonshapeactive);
-                    break;
-                case "correct":
-                    (findViewById(idList[i])).setBackgroundResource(R.drawable.buttonshapecorrect);
-                    break;
-                case "wrong":
-                    (findViewById(idList[i])).setBackgroundResource(R.drawable.buttonshapewrong);
-                    break;
+        //check if currentQuestion > numberQuestions
+        if (currentQuestion<numberOfQuestions) {
+            ((ImageView) findViewById(R.id.topImage)).setImageDrawable(images.getDrawable(currentQuestion));
+            // Update question and button resources
+            String[] currentAnswers = new String[4];
+            currentAnswers[0] = answersA[currentQuestion];
+            currentAnswers[1] = answersB[currentQuestion];
+            currentAnswers[2] = answersC[currentQuestion];
+            currentAnswers[3] = answersD[currentQuestion];
+
+            for (int i = 0; i < 4; i++) {
+                Button answerButton = findViewById(idList[i]);
+                answerButton.setText(currentAnswers[i]);
+            }
+            TextView questionTextView = findViewById(R.id.questionText);
+            questionTextView.setText(questions[currentQuestion]);
+
+            // Update button shape
+            for (int i = 0; i < 4; i++) {
+                switch (buttonStatus[i]) {
+                    case "none":
+                        (findViewById(idList[i])).setBackgroundResource(R.drawable.buttonshape);
+                        break;
+                    case "active":
+                        (findViewById(idList[i])).setBackgroundResource(R.drawable.buttonshapeactive);
+                        break;
+                    case "correct":
+                        (findViewById(idList[i])).setBackgroundResource(R.drawable.buttonshapecorrect);
+                        break;
+                    case "wrong":
+                        (findViewById(idList[i])).setBackgroundResource(R.drawable.buttonshapewrong);
+                        break;
+                }
+            }
+
+            //Define solution button string
+            Button solutionButton = findViewById(R.id.buttonSolution); //cast View to Button
+            if (solutionState.equals("next")) {
+                solutionButton.setText(R.string.next);
+            } else {
+                solutionButton.setText(R.string.solution);
             }
         }
     }
 
     public void displaySolution(View v) {
-        int[] answerNumber = new int[4];
-        if (android.text.TextUtils.isDigitsOnly(correctAnswers[currentQuestion]))
-        {
-            for(int i=0;i<4;i++)
-            {
-                answerNumber[i] = Integer.parseInt(correctAnswers[currentQuestion].substring(i,i+1));
+        //check if currentQuestion < numberQuestions => show questions
+        if (currentQuestion<numberOfQuestions) {
+            if (solutionState.equals("solution")) {
+                int[] answerNumber = new int[4];
+                //Single choice or multiple choice question
+                if (android.text.TextUtils.isDigitsOnly(correctAnswers[currentQuestion])) {
+                    for (int i = 0; i < 4; i++) {
+                        answerNumber[i] = Integer.parseInt(correctAnswers[currentQuestion].substring(i, i + 1));
+                    }
+                    for (int i = 0; i < 4; i++) {
+                        if (buttonStatus[i].equals("active")) {
+                            if (answerNumber[i] == 1)
+                                buttonStatus[i] = "correct";
+                            else
+                                buttonStatus[i] = "wrong";
+                        }
+                        (findViewById(idList[i])).setClickable(false);
+                    }
+                }
+                //Free answer mode
+                else {
+
+                }
+                solutionState = "next";
+            } else {
+                //Move to next question
+                currentQuestion +=1;
+                for(int i=0;i<4;i++) {
+                    buttonStatus[i] = "none"; //reset all buttons
+                    (findViewById(idList[i])).setClickable(true);
+                }
+                solutionState = "solution";
             }
         }
-        for(int i=0;i<4;i++){
-            if (buttonStatus[i].equals("active")) {
-                if (answerNumber[i]==1)
-                    buttonStatus[i] = "correct";
-                else
-                    buttonStatus[i] = "wrong";
-            }
+        //=> show evaluation
+        else
+        {
+
         }
         updateUI(v);
 
